2bd74659fd74970f5649bbd90c033ba8ffd4e12a
Xuemin Guan <xueminguan@xuemins-mbp.lan>
2022-06-20 16:49:31 +0100

    refactoring to re-use the logic dealing with book not found

--- app/src/main/java/com/sky/library/BookServiceImpl.java
+++ app/src/main/java/com/sky/library/BookServiceImpl.java
@@ -14,20 +14,22 @@ public class BookServiceImpl implements BookService {
         if (bookReference == null || !bookReference.startsWith(BOOK_REFERENCE_PREFIX))
             throw new InvalidBookReferencePrefixException(bookReference);
 
-        Book book = bookRepository.retrieveBook(bookReference);
-        if (book == null)
-            throw new BookNotFoundException(bookReference);
-
-        return book;
+        return retrieveBookOrError(bookReference);
     }
+
+
 
     @Override
     public String getBookSummary(String bookReference) throws BookNotFoundException {
-        Book book = bookRepository.retrieveBook(bookReference);
-
-        if (book == null)
-            throw new BookNotFoundException(bookReference);
+        Book book = retrieveBookOrError(bookReference);
 
         return String.format("[%s] %s - %s", bookReference, book.getTitle(), book.getReview());
+    }
+
+    private Book retrieveBookOrError(String bookReference) {
+        Book book = bookRepository.retrieveBook(bookReference);
+        if (book == null)
+            throw new BookNotFoundException(bookReference);
+        return book;
     }
 }
